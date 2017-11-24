326c36a10b0417de38d1e161d32120215409867f
Matt Davis <madavis111@outlook.com>
2017-11-23 19:51:06 -0800

    When someone wins, highlight the three squares that caused the win.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,18 +4,19 @@ import './index.css';
 
 function Square(props) {
   return (
-    <button className="square" onClick={props.onClick}>
+    <button className={"square" + (props.winningSquare ? ' highlight' : '')} onClick={props.onClick}>
       {props.value}
     </button>
   );
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
+  renderSquare(i, winningSquare) {
     return (
       <Square
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
+        winningSquare={winningSquare}
       />
     );
   }
@@ -37,7 +38,11 @@ class Board extends React.Component {
   getRows(i) {
     let rows = [];
     for (let j = 0; j <= 2; j++) {
-      rows.push(this.renderSquare(i * 3 + j));
+      let winningSquare;
+      if (this.props.winningSquares) {
+          winningSquare = this.props.winningSquares.indexOf(i * 3 + j) !== -1;
+      }
+      rows.push(this.renderSquare(i * 3 + j, winningSquare));
     }
     return rows;
   }
@@ -91,7 +96,7 @@ class Game extends React.Component {
       const history = this.state.history;
       const current = history[this.state.stepNumber];
       const winner = calculateWinner(current.squares);
-
+      console.log(winner);
       const moves = history.map((step, move) => {
           let point = getColumnAndRow(step.lastMove);
           let className = (move === this.state.stepNumber) ? 'current' : '';
@@ -106,9 +111,10 @@ class Game extends React.Component {
           );
       });
 
-    let status;
+    let status, winningSquares;
     if (winner) {
-        status = 'Winner: ' + winner;
+        status = 'Winner: ' + winner.player;
+        winningSquares = winner.moves;
     }
     else {
         status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
@@ -132,6 +138,7 @@ class Game extends React.Component {
           <Board
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
+            winningSquares={winningSquares}
           />
         </div>
         <div className="game-info">
@@ -173,7 +180,10 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return {
+          player: squares[a],
+          moves: [a, b, c],
+      }
     }
   }
   return null;
